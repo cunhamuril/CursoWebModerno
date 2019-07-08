21fed955e993f0f623e8e4b6ab113337e6df8062
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-07-08 14:15:57 -0300

    Componentes de Classe 01

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import Primeiro from './componentes/Primeiro'
-import Multi, { BoaNoite } from './componentes/Multiplos'
+// import Multi, { BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Ana"/>
-        <BoaNoite nome="Bia"/>
+        <Saudacao tipo="Bom dia" nome="João" />
     </div>
-, document.getElementById('root'))
+    , document.getElementById('root'))
