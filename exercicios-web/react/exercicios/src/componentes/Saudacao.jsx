84b9f8aa2a61cac640a1e9003eed9656fbecc72e
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-07-08 14:27:26 -0300

    Componentes de Classe 02

---
+++ exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class Saudacao extends Component {
+
+    state = {
+        tipo: this.props.tipo,
+        nome: this.props.nome
+    }
+    
+    constructor(props){
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)        
+    }
+
+    setTipo(e) {
+        // let i = 1
+        // setInterval(() => {
+        //     this.setState({ tipo: ++i })
+        // }, 1000);
+
+        // this.props.tipo = e.target.value
+        this.setState({ tipo: e.target.value })
+    }
+
+    setNome(e) {
+        this.setState({ nome: e.target.value })
+    }
+
+    render() {
+        const { tipo, nome } = this.state // antes de declarar o state estava assim: this.props
+        return (
+            <div>
+                <h1>{tipo} {nome}!</h1>
+                <hr />
+                <input type="text" placeholder="Tipo..."
+                    value={tipo} onChange={this.setTipo} />
+                <input type="text" placeholder="Nome..." 
+                    value={nome} onChange={(e) => this.setNome(e)} />
+            </div>
+        )
+    }
+}
