bfa2fc5c01cdf8c1607c82c6e8c67ca8ad2247d2
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-07-08 10:35:08 -0300

    Multiplos Componentes

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+import Multi, { BoaNoite } from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome="Murilo" />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana"/>
+        <BoaNoite nome="Bia"/>
+    </div>
+, document.getElementById('root'))
