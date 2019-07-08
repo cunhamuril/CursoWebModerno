75c5eea13fda728a8b7ea7d48ab987cbd23211d6
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-07-08 14:59:27 -0300

    Componentes pai e filho 02

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom'
 // import Multi, { BoaNoite } from './componentes/Multiplos'
 // import Saudacao from './componentes/Saudacao'
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
         <Pai nome="Paulo" sobrenome="Silva">
             {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
         </Pai>
     </div>
     , document.getElementById('root'))
